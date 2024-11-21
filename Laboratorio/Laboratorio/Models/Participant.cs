9ff943c38bd4399a7060e8c1efcc24c81f5587ef
Juan Gomez - Admin Account <jgadmin5@howdengroup.com>
2024-11-21 16:24:10 +0100

    Estructura base

---
+++ Laboratorio/Laboratorio/Models/Participant.cs
@@ -0,0 +1,11 @@
+﻿namespace Laboratorio.Models
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required ICollection<Event> Events { get; set; }
+    }
+}
