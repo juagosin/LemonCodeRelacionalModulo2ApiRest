3f09ddfecc9d29c0f846a2f3e89a6e251126db59
Juan Gomez - Admin Account <jgadmin5@howdengroup.com>
2024-11-22 09:54:00 +0100

    Creacion Contrato de los eventos

---
+++ Laboratorio/Laboratorio/Contracts/IEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using Laboratorio.Models;
+
+namespace Laboratorio.Contracts
+{
+    public interface IEventRepository
+    {
+        List<Event> GetEvents();
+        Event GetEvent(int id);
+        void CreateEvent(Event evento);
+        void UpdateEvent(Event evento);
+        void DeleteEvent(int id);
+
+    }
+}
