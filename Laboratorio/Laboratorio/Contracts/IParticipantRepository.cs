5e716d283610eb86032189bbc1fb18b450b08613
Juan Gomez - Admin Account <jgadmin5@howdengroup.com>
2024-11-22 10:02:30 +0100

    Participant Repository

---
+++ Laboratorio/Laboratorio/Contracts/IParticipantRepository.cs
@@ -0,0 +1,14 @@
+﻿using Laboratorio.Models;
+
+namespace Laboratorio.Contracts
+{
+    public interface IParticipantRepository
+    {
+        List<Participant> GetParticipants();
+        Participant GetParticipant(int id);
+        void CreateParticipant(Participant participant);
+        void UpdateParticipant(Participant participant);
+        void DeleteParticipant(int id);
+
+    }
+}
