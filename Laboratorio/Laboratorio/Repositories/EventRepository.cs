acbcde757e7564eb5433f6034be85fdcc9ceee08
Juan Gomez - Admin Account <jgadmin5@howdengroup.com>
2024-11-22 10:33:03 +0100

    Creacion de los repositorios

---
+++ Laboratorio/Laboratorio/Repositories/EventRepository.cs
@@ -0,0 +1,33 @@
+﻿using Laboratorio.Contracts;
+using Laboratorio.Models;
+
+namespace Laboratorio.Repositories
+{
+    public class EventRepository : IEventRepository
+    {
+        public void CreateEvent(Event evento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEvent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Event GetEvent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event> GetEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEvent(Event evento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Laboratorio/Laboratorio/Repositories/ParticipantRepository.cs
@@ -0,0 +1,33 @@
+﻿using Laboratorio.Contracts;
+using Laboratorio.Models;
+
+namespace Laboratorio.Repositories
+{
+    public class ParticipantRepository : IParticipantRepository
+    {
+        public void CreateParticipant(Participant participant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteParticipant(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Participant GetParticipant(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Participant> GetParticipants()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateParticipant(Participant participant)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
