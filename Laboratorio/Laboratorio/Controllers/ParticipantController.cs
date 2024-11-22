a01893f75bbb53f27e32c506a371c3cc2efc8c88
Juan Gomez - Admin Account <jgadmin5@howdengroup.com>
2024-11-22 12:52:12 +0100

    Participant controller

---
+++ Laboratorio/Laboratorio/Controllers/ParticipantController.cs
@@ -0,0 +1,25 @@
+﻿using Laboratorio.Contracts;
+using Laboratorio.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParticipantController : ControllerBase
+    {
+        private readonly IParticipantRepository _participantRepository;
+        public ParticipantController(IParticipantRepository participantRepository)
+        {
+            _participantRepository = participantRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Participant>> Get()
+        {
+            var participants = _participantRepository.GetParticipants();
+            return Ok(participants);
+        }
+    }
+}
